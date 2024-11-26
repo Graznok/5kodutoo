be2936b3a78ed7c37c8445761944d222fd97537d
Anu Tursk <66010207+Graznok@users.noreply.github.com>
2024-11-26 08:33:50 +0200

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,9 @@
+<nav class="navbar fixed-bottom bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand ms-5" href="#">AnuTursk</a>
+        </div>
+    </nav> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
